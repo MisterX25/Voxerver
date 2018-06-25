94f247bf833eea21820c70283f43aaa842f73ea3
MisterX25 <Xavier.Carrel@cpnv.ch>
2018-06-25 12:03:29 +0200

    added random errors

--- app/Http/Controllers/AssignmentController.php
+++ app/Http/Controllers/AssignmentController.php
@@ -9,11 +9,37 @@ class AssignmentController extends Controller
 {
     public function assignmentsFor($token)
     {
+        // Check if user has some assignment not done yet
+        $notDone = \DB::table('users')
+            ->join('personalAssignments', 'users.id', '=', 'personalAssignments.user_id')
+            ->join('classAssignments', 'classAssignments.id', '=', 'personalAssignments.classAssignment_id')
+            ->select('personalAssignments.id as assignment_id')
+            ->where('token', '=', $token)->where('personalAssignments.result', '=', null)
+            ->first();
+        if (!$notDone)
+        {
+            $v = rand(0,10);
+            error_log("Mmmmm....$v");
+            if ($v < 5)
+            {
+                if ($v < 3)
+                    abort(403, 'Je suis en pause....');
+                else
+                    abort(500, "Ca peut arriver...");
+                return;
+                error_log("Zbang....");
+            }
+        }
+        else
+        {
+            error_log("Bosse....");
+        }
+
         return json_encode(\DB::table('users')
             ->join('personalAssignments', 'users.id', '=', 'personalAssignments.user_id')
             ->join('classAssignments', 'classAssignments.id', '=', 'personalAssignments.classAssignment_id')
             ->join('vocabularies', 'vocabularies.id', '=', 'classAssignments.voc_id')
-            ->select('personalAssignments.id as assignment_id','vocabularies.id as vocabulary_id', 'vocabularies.title')
+            ->select('personalAssignments.id as assignment_id','vocabularies.id as vocabulary_id', 'vocabularies.title', 'personalAssignments.result')
             ->where('token', '=', $token)
             ->get());
     }
