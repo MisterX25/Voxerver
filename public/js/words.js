52e99688fca9d4f8082d895ebd51c5910428b798
MisterX25 <Xavier.Carrel@cpnv.ch>
2019-02-01 13:28:58 +0100

    For exam

--- public/js/words.js
+++ public/js/words.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
         value = $(this).text();
         $('input[name="selectfirstword"]').val(value);
         $('div.selectfirstword').text(value);
+        $('#idlang').val($(this).data('idlang')); // take the data-idlang attribute of the <td> and put it in the hidden form field
+        $('#idsem').val($(this).data('idsem'));
         console.log(this);
     });
     $("table tr th").click(function(){
--- resources/views/word.blade.php
+++ resources/views/word.blade.php
@@ -14,20 +14,24 @@
                         <th scope="col">{{$language->languageName}}</th>
                     @endforeach
                     </tr>
-                    @foreach($res as $key => $word)
+                    @foreach($res as $semantic => $word)
                         <tr>
                         @foreach($languages as $language)
-                        <td>{{$word[$language->id]}}</td>
+                        <td data-idlang="{{ $language->id }}" data-idsem="{{ $semantic }}">{{$word[$language->id]}}</td>
                         @endforeach
                         </tr>
                     @endforeach
                 </table>
             </div>
+            <form method="post" action="/defword">
             <div class="addword">
                 <div class="selectfirstword">...</div> en <div class="selectlanguage">...</div> est
-                <input type="text" class="newword">
+                <input type="text" class="newword" name="value">
+                <input type="hidden" name="idlang" id="idlang">
+                <input type="hidden" name="idsem" id="idsem">
                 <button type="submit" name="addnewword" class="btn btn-success">Ajouter</button>
             </div>
+            </form>
         </div>
     </div>
     <script type="text/javascript" src="{{ asset('js/words.js') }}"></script>
